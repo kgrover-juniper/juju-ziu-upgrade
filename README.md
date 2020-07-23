4b0d7335ac0f0872067022d7db22485f79fbe237
K Grover <myemail@juniper.net>
2020-07-23 00:09:02 -0700

    update

---
+++ README.md
@@ -0,0 +1,18 @@
+i# juju-ziu-upgrade
+Required files:
+1. juju-ziu.go
+2. controller-upgrade.sh
+3. agent-upgrade.sh
+
+
+Command line execution:
+-----------------------
+go run juju-ziu.go 'contrail-build'
+
+'contrail-build'
+ 2008.12 
+ 
+ Output:
+ -------
+ result.txt
+ #Comment on/off write_result() in juju-ziu.go
--- juju-ziu.go
+++ juju-ziu.go
@@ -4,72 +4,19 @@ import(
         "fmt"
         "os/exec"
         "os"
-        "bufio"
         "log"
         "time"
-	"regexp"
+	"strconv"
 	"strings"
 )
 
 var (
-	minutes int = 60
-	stage int
-	upgraded int
-	total_count int
-	deployed int
-	result string = ""
+	start time.Time
+	end time.Time
+	minutes int = 30
+	message string
 )
 
-func check_status() {
-	cmd := exec.Command("juju", "status")
-        stdout, err := cmd.Output()
-        check_error(err)
-
-        file := "status.txt"
-        remove_previous(file)
-        recent_status, err_open := os.OpenFile(file, os.O_RDWR|os.O_CREATE, 0644)
-        check_error(err_open)
-        defer recent_status.Close()
-	_, err_output := fmt.Fprintln(recent_status, string(stdout))
-        check_error(err_output)
-
-}
-
-func verify_deployment(mins int) (int,string){
-        tries := 0
-        status_verified := false
-        for (status_verified==false && tries<=mins){
-                tries++
-                total_count = 0
-                deployed = 0
-                time.Sleep(60*time.Second) //sleep for a minute before checking the juju status
-                check_status()//check status after a minute
-                fin, err_open_file := os.Open("status.txt")
-                check_error(err_open_file)
-                defer fin.Close()
-                scanner := bufio.NewScanner(fin)
-                for scanner.Scan() {
-                        total_count += 1
-                        line := scanner.Text()
-                        in_progress := regexp.MustCompile(`allocating|pending|waiting|blocked|executing|maintenance`)
-                        in_error := regexp.MustCompile(`error`)
-                        if in_error.MatchString(line)==true {
-                                return -1, "Error, "
-                        }
-                        if in_progress.MatchString(line)==false {
-                                deployed += 1
-                        }
-                }
-                if total_count == deployed {
-                        status_verified = true
-                }
-        }
-        if !status_verified{
-                return -1,"Tries Expired, "
-        } else {
-                return 0,""
-        }
-}
 
 func check_error(e error) {
     if e != nil {
@@ -77,58 +24,68 @@ func check_error(e error) {
     }
 }
 
-func remove_previous(filename string) {
-	if file_exists(filename) {
-		err_file := os.Remove(filename)
-                check_error(err_file)
+func verify_deployment(min int) (int, string, int) {
+        tries := 0
+        status_verified := false
+        for (status_verified==false && tries<min){
+                tries++
+                time.Sleep(60*time.Second) //sleep for a minute before checking the juju status
+                c2 := exec.Command("grep", "-e allocating", "-e blocked", "-e pending", "-e waiting", "-e maintenance", "-e executing", "-e error")
+                c1 := exec.Command("juju", "status")
+                pipe, _ := c1.StdoutPipe()
+                defer pipe.Close()
+                c2.Stdin = pipe
+                c1.Start()
+                stdout, _ := c2.Output()
+                if string(stdout) == "" {
+                        status_verified = true
+                }
+                _ = c1.Wait()
+        }
+        if !status_verified{
+                return -1,"Tries Expired, ",tries
+        } else {
+                return 0,"",tries
         }
 }
 
-func file_exists(fileName string) (bool) {
-    _, err := os.Stat(fileName)
-    if os.IsNotExist(err) {
-        return false
-    } else {
-            return true
-    }
-}
 
-func verify_upgrade() (int,string){
+func verify_upgrade() (int,string,int){
         tries := 0
         status_verified := false
         for (status_verified==false && tries<=minutes){
                 tries++
-                stage = 0
-                upgraded = 0
+		stages := 0
+		completed := 0
                 time.Sleep(60*time.Second) //sleep for a minute before checking the juju status
-                check_status()//check status after a minute
-                fin, err_open_file := os.Open("status.txt")
-                check_error(err_open_file)
-                defer fin.Close()
-                scanner := bufio.NewScanner(fin)
-                for scanner.Scan() {
-                        line := scanner.Text()
-			if strings.Contains(line,"stage/done = "){
-				stage += 1
-				words := strings.FieldsFunc(line, func(c rune) bool {
-                                        return c == ' '
-                                })
-				for i:=0;i<len(words);i++{
-                                        if (words[i]=="=" && words[i+1]=="5/5"){
-                                                upgraded += 1
-                                                break
-                                        }
-                                }
+		c2 := exec.Command("grep", "stage/done")
+                c1 := exec.Command("juju", "status")
+                pipe, _ := c1.StdoutPipe()
+                defer pipe.Close()
+                c2.Stdin = pipe
+                c1.Start()
+                stdout, _ := c2.Output()
+		output := string(stdout)
+                if output != "" {
+			lines := strings.Split(output,"\n")
+			for each:=0 ; each<len(lines); each++ {
+				if(lines[each] != "") {
+					stages++
+					if strings.Contains(lines[each], "5/5") {
+						completed++
+					}
+				}
+			}
+			if stages == completed {
+				status_verified = true
 			}
                 }
-                if stage == upgraded {
-                        status_verified = true
-                }
+                _ = c1.Wait()
         }
         if !status_verified{
-                return -1,"Tries Expired, "
+                return -1,"Tries Expired, ",tries
         } else {
-                return 0,""
+                return 0,"",tries
         }
 }
 
@@ -136,46 +93,45 @@ func upgrade_procedure() string {
 	cmd := exec.Command("/bin/sh","-x", "controller-upgrade.sh", os.Args[1])
         err := cmd.Run()
         check_error(err)
-	return_code,return_message := verify_upgrade()
+	return_code,return_message,time := verify_upgrade()
 	if return_code == -1 {
-		return_message += " Controller Upgrade Failed\n"
+		return_message += "Controller Upgrade Failed in approx ~" + strconv.Itoa(time) + " minutes\n"
 	} else {
+		return_message += "Controller Upgrade Successful in approx ~" + strconv.Itoa(time) + " minutes\n"
 		c := exec.Command("/bin/sh","-x", "agent-upgrade.sh")
 		e := c.Run()
 		check_error(e)
-		rc,rm :=  verify_deployment(60)
+		rc,rm,t :=  verify_deployment(30)
 		if rc == -1 {
-			return_message += rm + " Computes Upgrade Failed\n"
+			return_message += rm + "Computes Upgrade Failed in approx ~" + strconv.Itoa(t) + " minutes\n"
 		} else {
-			return_message += rm + " All upgrade successful\n"
+			return_message += rm + "Computes Upgrade Successful in approx ~" + strconv.Itoa(t) + " minutes\n"
 		}
 	}
 	return return_message
 }
 
 func zero_impact_upgrade(){
-	start := time.Now()
-        return_code,return_message := verify_deployment(1)
+	start = time.Now()
+        return_code,return_message,_ := verify_deployment(1)
 
         if return_code == -1{
-                return_message += "\n Previously faulty deployment, Upgrade not possible"
+                message = "\nPreviously faulty deployment, Upgrade not possible"
         } else {
 		upgrade_result := upgrade_procedure()
-                return_message += upgrade_result
+                message = return_message + upgrade_result
         }
-	end := time.Now()
-	elapsed_time := end.Sub(start)
-
-	result += "\nJuju Zero Impact Upgrade - " + os.Args[1]
-	result += return_message
-	result += "\nStarted at " + start.String()
-	result += "\nEnded at " + end.String()
-	result += "\nTime taken = " + elapsed_time.String()
+	end = time.Now()
 }
 
 func write_result() {
+	result := "\nJuju Zero Impact Upgrade to " + os.Args[1] + "\nStarted at " + start.String() + "\nEnded at " + end.String()
+	result += "\nTime taken = " + end.Sub(start).String() + "\n" + message
 	file := "result.txt"
-        remove_previous(file)
+	if _, err := os.Stat(file); !os.IsNotExist(err) {
+                err_file := os.Remove(file)
+                check_error(err_file)
+        }
         output, err_open := os.OpenFile(file, os.O_RDWR|os.O_CREATE, 0644)
         check_error(err_open)
         defer output.Close()
